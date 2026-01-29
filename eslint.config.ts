6ba90c640a210ce5c7600e71b9951f0f4cca279c
sergios <sergios@gmail.com>
2026-01-29 11:24:20 +0100

    fix: add TypeScript rule to prevent unused variables in ESLint configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -14,6 +14,17 @@ export default defineConfig([
   },
   ...tseslint.configs.recommended,
   {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { 
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        }
+      ]
+    }
+  },
+  {
     ignores: ["dist/**", "node_modules/**"],
   },
 ]);
